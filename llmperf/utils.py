8536b0dfae91d74aa90780da912e619a062c95cb
Konstantinos Papaioannou <konstantinos.papaioannou@imdea.org>
2025-05-16 11:58:28 +0200

    Update static workload runner script
    Signed-off-by: Konstantinos Papaioannou <konstantinos.papaioannou@imdea.org>

--- llmperf/utils.py
+++ llmperf/utils.py
@@ -1,4 +1,154 @@
 from PIL import Image, ImageFile
+from transformers import AutoProcessor, AutoTokenizer
+from typing import Dict
+
+from vllm.assets.audio import AudioAsset
+from vllm.assets.video import VideoAsset
+
+from llmperf.config.models import Model
+from llmperf.config.workloads import Request
 
 def load_image(path: str) -> ImageFile.ImageFile:
     return Image.open(path)
+
+def duration_to_frames(duration, min_duration=1, max_duration=180, min_frames=8, max_frames=64):
+    duration = max(min_duration, min(max_duration, duration))
+    scale = (duration - min_duration) / (max_duration - min_duration)
+    frames = min_frames + scale * (max_frames - min_frames)
+    return int(frames)
+
+def prepare_text_prompt(request: Request, model: Model) -> Dict:
+    tokenizer = AutoTokenizer.from_pretrained(model.path)
+    prompt = [
+        {"role": "user", "content": request.input}
+    ]
+    formatted_prompt = tokenizer.apply_chat_template(
+        prompt,
+        add_generation_prompt=True,
+        tokenize=False
+    )
+    
+    final_prompt = {
+        "prompt": formatted_prompt
+    }
+    return final_prompt
+
+def prepare_image_prompt(request: Request, model: Model) -> Dict:
+    processor = AutoProcessor.from_pretrained(model.path)
+    prompt = [
+        {
+            "role": "user",
+            "content": [
+                {"type": "image"},
+                {"type": "text", "text": request.input}
+            ]
+        }
+    ]
+    formatted_prompt = processor.apply_chat_template(
+        prompt,
+        add_generation_prompt=True,
+        tokenize=False
+    )
+    
+    image = load_image(request.modality_path)
+
+    final_prompt = {
+        "prompt": formatted_prompt,
+        "multi_modal_data": {"image": image}
+    }
+    return final_prompt
+
+def prepare_video_prompt(request: Request, model: Model) -> Dict:
+    processor = AutoProcessor.from_pretrained(model.path)
+    prompt = [
+        {
+            "role": "user",
+            "content": [
+                {"type": "video"},
+                {"type": "text", "text": request.input}
+            ]
+        }
+    ]
+    formatted_prompt = processor.apply_chat_template(
+        prompt,
+        add_generation_prompt=True,
+        tokenize=False
+    )
+    
+    num_available_frames = request.modality_size["frame_count"]
+    if num_available_frames == 0:
+        return {}
+
+    num_frames = duration_to_frames(request.modality_size["duration"])
+    num_frames = num_available_frames if num_available_frames < num_frames else num_frames
+    
+    video = VideoAsset(name=request.modality_path, num_frames=num_frames).np_ndarrays
+
+    final_prompt = {
+        "prompt": formatted_prompt,
+        "multi_modal_data": {"video": video}
+    }
+    return final_prompt
+
+def prepare_audio_prompt(request: Request, model: Model) -> Dict:
+    processor = AutoProcessor.from_pretrained(model.path)
+    audio_in_prompt = "".join([
+        f"Audio 1: "
+        f"<|audio_bos|><|AUDIO|><|audio_eos|>\n"
+    ])
+    prompt = [
+        {
+            "role": "user",
+            "content": [
+                {"type": "audio"},
+                {"type": "text", "text": audio_in_prompt + request.input}
+            ]
+        }
+    ]
+    formatted_prompt = processor.apply_chat_template(
+        prompt,
+        add_generation_prompt=True,
+        tokenize=False
+    )
+
+    audio = AudioAsset(request.modality_path).audio_and_sample_rate
+
+    final_prompt = {
+        "prompt": formatted_prompt,
+        "multi_modal_data": {"audio": audio}
+    }
+    return final_prompt
+
+def get_modality_token_index(request: Request, model: Model) -> int:
+    image_codecs = {"JPEG"}
+    video_codecs = {"h264", "vp6f", "vp9"}
+    audio_codecs = {"pcm_s16le"}
+    
+    if request.modality_path is None:
+        return -1 
+
+    if request.modality_size["codec"] in image_codecs:
+        return model.image_token_index or -1
+    elif request.modality_size["codec"] in video_codecs:
+        return model.video_token_index or -1
+    elif request.modality_size["codec"] in audio_codecs:
+        return model.audio_token_index or -1
+    else:
+        return -1
+    
+def prepare_final_prompt(request: Request, model: Model) -> Dict:
+    modality_token_index = get_modality_token_index(request, model)
+
+    if modality_token_index == model.image_token_index:
+        final_prompt = prepare_image_prompt(request, model)
+
+    elif modality_token_index == model.video_token_index:
+        final_prompt = prepare_video_prompt(request, model)
+
+    elif modality_token_index == model.audio_token_index:
+        final_prompt = prepare_audio_prompt(request, model)
+
+    else:
+        final_prompt = prepare_text_prompt(request, model)
+
+    return final_prompt
--- scripts/run_static_workloads.py
+++ scripts/run_static_workloads.py
@@ -1,181 +1,91 @@
+import argparse
 import time
 
 from datetime import datetime
-from transformers import AutoProcessor, AutoTokenizer
+from transformers import AutoTokenizer
 from tqdm import tqdm
 from vllm import LLM, SamplingParams
-from vllm.assets.audio import AudioAsset
-from vllm.assets.video import VideoAsset
 
-from llmperf.config.approaches import get_approach_by_name
-from llmperf.config.models import get_model_by_name
-from llmperf.config.workloads import get_workload_by_name
+from llmperf.config.approaches import get_approach_by_alias
+from llmperf.config.models import get_model_by_alias
+from llmperf.config.workloads import get_workload_by_alias
 from llmperf.postprocessing.output import RequestOutput, ExperimentOutput
-from llmperf.utils import load_image
+from llmperf.utils import get_modality_token_index, prepare_final_prompt
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Parse resource usage and experiment metadata."
+    )
+    
+    parser.add_argument('--gpu-util', type=float, default=0.95,
+                        help='GPU utilization percentage (default: 0.95)')
+    parser.add_argument('--swap-space', type=int, default=0,
+                        help='Swap space used in GB (default: 0)')
+    parser.add_argument('--model', type=str, required=True,
+                        help='Model name (e.g., LLaVA-Onevision-7b)')
+    parser.add_argument('--workload', type=str, required=True,
+                        help='Workload type (e.g., Video Description)')
+
+    return parser.parse_args()
 
 if __name__ == '__main__':
+    args = parse_args()
+
     START_TIME = datetime.now().strftime("%Y%m%d-%H%M%S")
-    FRAMES = 64
 
-    GPU_UTIL = 0.95
-    SWAP_SPACE = 0
-    approach = get_approach_by_name("Isolation")
-    
-    workload_names = [
-        "Text Conversations",
-        "Image Reasoning",
-        "Video Description",
-        "Audio Captioning"
-    ]
+    approach = get_approach_by_alias("iso")
+    model = get_model_by_alias(args.model)
+    workload = get_workload_by_alias(args.workload)
+    workload.load()
 
-    model_names = [
-        "Mistral-7b",
-        "LLaVA 1.6 (Mistral-7b)",
-        "LLaVA-Next-Video (Mistral-7b)",
-        "Qwen2-Audio-7b"
-    ]
+    try:
+        llm = LLM(
+            model=model.path,
+            gpu_memory_utilization=args.gpu_util,
+            swap_space=args.swap_space,
+            scheduling_policy=approach.scheduling_policy,
+            disable_log_stats=False,
+            max_model_len=model.max_model_len,
+            max_num_batched_tokens=model.max_model_len,
+            num_gpu_blocks_override=model.max_model_len // 16
+        )
 
-    for workload_name, model_name in zip(workload_names, model_names):
-        try:
-            model = get_model_by_name(model_name)
+        requests = workload.requests
+        tokenizer = AutoTokenizer.from_pretrained(model.path)
 
-            llm = LLM(
-                model=model.path,
-                gpu_memory_utilization=GPU_UTIL,
-                swap_space=SWAP_SPACE,
-                scheduling_policy=approach.scheduling_policy,
-                enable_custom_scheduler=approach.enable_custom_scheduler,
-                enable_chunked_prefill=approach.enable_chunked_prefill
+        outputs = []
+        start_time = now = time.time()
+        for request in tqdm(requests):
+            output_length = len(tokenizer.encode(request.output))
+
+            sampling_params = SamplingParams(
+                ignore_eos=True,
+                max_tokens=int(output_length)
             )
 
-            workload = get_workload_by_name(workload_name)
-            workload.load()
-            requests = workload.requests
+            modality_token_index = get_modality_token_index(request, model)
+            final_prompt = prepare_final_prompt(request, model)
 
-            outputs = []
-            modality_token_index = -1
-            start_time = time.time()
-            for request in tqdm(requests):
-                tokenizer = AutoTokenizer.from_pretrained(model.path)
-                output_length = len(tokenizer.encode(request.output))
+            req_output = llm.generate(
+                prompts=[final_prompt],
+                sampling_params=sampling_params,
+                use_tqdm=False
+            )[0]
 
-                sampling_params = SamplingParams(
-                    ignore_eos=True,
-                    max_tokens=int(output_length)
+            now = time.time()
+            outputs.append(
+                RequestOutput.from_vllm_output(
+                    request.id, req_output, modality_token_index
                 )
-
-                if workload.alias == "text-static":
-                    modality_token_index = -1
-                    tokenizer = AutoTokenizer.from_pretrained(model.path)
-                    prompt = [
-                        {"role": "user", "content": request.input}
-                    ]
-                    formatted_prompt = tokenizer.apply_chat_template(prompt, add_generation_prompt=True, tokenize=False)
-                    final_prompt = {
-                        "prompt": formatted_prompt
-                    }
-
-                if workload.alias == "image-static":
-                    modality_token_index = model.image_token_index
-                    processor = AutoProcessor.from_pretrained(model.path)
-                    prompt = [
-                        {
-                            "role": "user",
-                            "content": [
-                                {"type": "image"},
-                                {"type": "text", "text": request.input}
-                            ]
-                        }
-                    ]
-                    formatted_prompt = processor.apply_chat_template(
-                        prompt,
-                        add_generation_prompt=True,
-                        tokenize=False
-                    )
-                    
-                    image = load_image(request.modality_path)
-
-                    final_prompt = {
-                        "prompt": formatted_prompt,
-                        "multi_modal_data": {"image": image}
-                    }
-
-                if workload.alias == "video-static":
-                    modality_token_index = model.video_token_index
-                    processor = AutoProcessor.from_pretrained(model.path)
-                    prompt = [
-                        {
-                            "role": "user",
-                            "content": [
-                                {"type": "video"},
-                                {"type": "text", "text": request.input}
-                            ]
-                        }
-                    ]
-                    formatted_prompt = processor.apply_chat_template(
-                        prompt,
-                        add_generation_prompt=True,
-                        tokenize=False
-                    )
-
-                    video = VideoAsset(name=request.modality_path, num_frames=FRAMES).np_ndarrays
-
-                    final_prompt = {
-                        "prompt": formatted_prompt,
-                        "multi_modal_data": {"video": video}
-                    }
-
-                if workload.alias == "audio-static":
-                    modality_token_index = model.audio_token_index
-                    processor = AutoProcessor.from_pretrained(model.path)
-
-                    audio_in_prompt = "".join([
-                        f"Audio 1: "
-                        f"<|audio_bos|><|AUDIO|><|audio_eos|>\n"
-                    ])
-                    prompt = [
-                        {
-                            "role": "user",
-                            "content": [
-                                {"type": "audio"},
-                                {"type": "text", "text": audio_in_prompt + request.input}
-                            ]
-                        }
-                    ]
-                    formatted_prompt = processor.apply_chat_template(
-                        prompt,
-                        add_generation_prompt=True,
-                        tokenize=False
-                    )
-
-                    audio = AudioAsset(request.modality_path).audio_and_sample_rate
-
-                    final_prompt = {
-                        "prompt": formatted_prompt,
-                        "multi_modal_data": {"audio": audio}
-                    }
-
-                req_output = llm.generate(
-                    prompts=[final_prompt],
-                    sampling_params=sampling_params,
-                    use_tqdm=False
-                )[0]
-
-                now = time.time()
-                outputs.append(
-                    RequestOutput.from_vllm_output(
-                        request.id, req_output, modality_token_index
-                    )
-                )
-
-        finally:
-            elapsed_time = now - start_time
-
-            experiment_output = ExperimentOutput(
-                id=f"{workload.alias}-{model.alias}-{approach.alias}-{START_TIME}",
-                elapsed_time=elapsed_time,
-                request_outputs=outputs
             )
-            experiment_output.save()
 
-            llm = None
+    finally:
+        elapsed_time = now - start_time
+
+        experiment_output = ExperimentOutput(
+            id=f"{workload.alias}-{model.alias}-{approach.alias}-{START_TIME}",
+            elapsed_time=elapsed_time,
+            request_outputs=outputs
+        )
+        experiment_output.save()
+        print(f"Saved {experiment_output.id}")
